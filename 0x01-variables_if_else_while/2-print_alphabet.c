9357b3164ac149962c3ef9344d84889724193604
ViviEsther <vivianaesther@gmail.com>
2022-02-11 23:10:18 +0000

    I sometimes suffer from insomnia...

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+*main - Write a program that prints the alphabet in lowercase.
+*
+*Return: Always 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+		printf("\n")
+
+	return (0);
+}
