45ec41ccfe031a71f5b7cf622cb05ca1ac04db66
ViviEsther <vivianaesther@gmail.com>
2022-02-12 00:52:45 +0000

    Write a program that prints all single digit...

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Write a program that prints all single digit..
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+			printf("\n");
+
+	return (0);
+}
