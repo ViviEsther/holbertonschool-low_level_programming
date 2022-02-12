29120ca7ae2105e45e1641cab995926ecce9e6a0
ViviEsther <vivianaesther@gmail.com>
2022-02-12 02:02:55 +0000

    Write a program that prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Write a program that prints all single digit numbers...
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (n = 65; n < 71; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
