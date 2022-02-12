d327d289083eadf3753e463c4d15fa7591080abb
ViviEsther <vivianaesther@gmail.com>
2022-02-12 01:40:53 +0000

    Write a program that prints all single digit numbers of base 10 again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Write a program that prints all single digit numbers...
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
