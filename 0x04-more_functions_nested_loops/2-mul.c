1fb3e06f2433e1833d0045f494891f1dbcfbfaa4
ViviEsther <vivianaesther@gmail.com>
2022-02-17 16:30:13 +0000

    Function that prints atwo integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - function that multiplies two integers
+* @a: Input 1
+* @b: Input 2
+* Return: Always x
+*/
+
+int mul(int a, int b)
+{
+	int x;
+		x = (a * b);
+	return (x);
+}
