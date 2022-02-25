01aa3f03a85cb2abaee706b69acbfcb652c168c1
ViviEsther <vivianaesther@gmail.com>
2022-02-25 00:26:51 +0000

    Function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*cap_string - Function that capitalizes all words of a string
+*@s: string of letteres
+*Return: s
+*/
+
+char *cap_string(char *s)
+{
+	char *str = s;
+	char del[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	int count;
+
+	while (*s)
+	{
+		count = 0;
+		while (*(del + count))
+		{
+			if (*s == *(del + count))
+			{
+				if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
+					*(s + 1) = *(s + 1) - 32;
+			}
+			count++;
+		}
+		s = s + 1;
+	}
+	return (str);
+}
